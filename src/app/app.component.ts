b7d8388afed2aa19b78ae49072348ebb8e8fd443
muyncio <muynek@gmail.com>
2022-09-12 12:26:14 +0200

    adding tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import {Task} from "./task";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'fun-with-angular';
+  config: { [key: string]: string } | null  = null;
+  task : Task[] = [{
+    name: 'Siłka',
+    deadline: '2022-09-09',
+    done: false
+  },
+    {
+      name: 'Nauka Angulara',
+      deadline: '2022-09-09',
+      done: true
+    },
+    {
+      name: 'Wycieczka do Gorzowa',
+      deadline: '2022-09-10',
+      done: true
+    },
+    {
+      name: 'Siłka',
+      deadline: '2022-09-12',
+      done: false
+    }
+];
+
+  constructor() {
+      this.config = {
+        title: 'Lista zadań',
+        footer: '© Lista zadań zbudowana w Angularze.',
+        date: new Date().toDateString()
+      };
+  }
 }
